7a273de790b51a26ce26cb9071af7fc904a6a25d
misheck gogo <misheckgogo45@gmail.com>
2024-04-09 10:26:07 -0700

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,41 +10,41 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid;
 
-    if (array == NULL || size == 0)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    while (left <= right)
-    {
-        mid = (left + right) / 2;
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
 
-        printf("Searching in array: ");
-        for (size_t i = left; i <= right; i++)
-        {
-            printf("%d", array[i]);
-            if (i != right)
-            {
-                printf(", ");
-            }
-        }
-        printf("\n");
+		printf("Searching in array: ");
+		for (size_t i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i != right)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
 
-        if (value == array[mid])
-        {
-            return (mid);
-        }
-        else if (value < array[mid])
-        {
-            right = mid - 1;
-        }
-        else
-        {
-            left = mid + 1;
-        }
-    }
+		if (value == array[mid])
+		{
+			return (mid);
+		}
+		else if (value < array[mid])
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
 
-    return (-1);
+	return (-1);
 }
